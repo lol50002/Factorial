dd288b0544813711381abe996d484afa3b2f0dab
lol50002 <artem130500@mail.ru>
2016-04-16 21:55:42 +0300

    :+1New
    New

---
+++ Factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32 b;
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Fib(1,b));
+            Console.ReadKey();
+        }
+
+
+        public static int Fib(int result,int n)
+        {
+            if (n <= 1) return result;
+            return Fib(result * n, n=n-1);
+        }
+    }
+}
